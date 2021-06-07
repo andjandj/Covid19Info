d1fd09e78406437c19dc20d06330c7eb7721685f
Luka <dogandzic97@gmail.com>
2021-06-07 15:28:16 +0200

    Commit Staticki Podaci html+css

--- Covid19Info/Controllers/HomeController.cs
+++ Covid19Info/Controllers/HomeController.cs
@@ -33,7 +33,10 @@ namespace Covid19Info.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
+        public IActionResult StatistickiPodaci()
+        {
+            return View();
+        }
 
     }
 }
