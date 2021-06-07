e518c1df412f3a8b0a7bf70cbf1dfaa6adf46761
Luka <dogandzic97@gmail.com>
2021-06-07 11:07:00 +0200

    Commit BrojZarazenihOsoba CRUD

---
+++ Covid19Info/Controllers/BrojZarazenihController.cs
@@ -0,0 +1,110 @@
+﻿using Covid19Info.Models;
+using Covid19Info.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19Info.Controllers
+{
+    public class BrojZarazenihController : Controller
+    {
+        private readonly Covid19InfoContext _context;
+
+        public BrojZarazenihController(Covid19InfoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<BrojZarazenihOsoba> brOsobaLista = _context.BrojZarazenihOsobas.ToList();
+            return View(brOsobaLista);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BrojZarazenihOsobaViewModel model)
+        {
+            BrojZarazenihOsoba brZarazenih = new BrojZarazenihOsoba
+            {
+                BrojTestiranihOsobaDnevno = model.BrojTestiranihOsobaDnevno,
+                BrojPotvrdjenihSlucajeva = model.BrojPotvrdjenihSlucajeva,
+                BrojSmrtnihSlucajeva = model.BrojSmrtnihSlucajeva,
+                UkupanBrojTestiranihSlucajeva = model.UkupanBrojTestiranihSlucajeva,
+                DatumTestiranja = model.DatumTestiranja,
+                DeoNaKojiSePodaciOdnose = model.DeoNaKojiSePodaciOdnose,
+
+            };
+            try
+            {
+                _context.BrojZarazenihOsobas.Add(brZarazenih);
+                _context.SaveChanges();
+                ViewBag.Msg = "Uspesno ubaceno";
+                return View();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(_context.BrojZarazenihOsobas.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, BrojZarazenihOsobaViewModel model)
+        {
+            BrojZarazenihOsoba brZarazenih = _context.BrojZarazenihOsobas.Find(id);
+
+            brZarazenih.BrojPotvrdjenihSlucajeva = model.BrojPotvrdjenihSlucajeva;
+            brZarazenih.BrojSmrtnihSlucajeva = model.BrojSmrtnihSlucajeva;
+            brZarazenih.BrojTestiranihOsobaDnevno = model.BrojTestiranihOsobaDnevno;
+            brZarazenih.DatumTestiranja = model.DatumTestiranja;
+            brZarazenih.DeoNaKojiSePodaciOdnose = model.DeoNaKojiSePodaciOdnose;
+            brZarazenih.UkupanBrojTestiranihSlucajeva = model.UkupanBrojTestiranihSlucajeva;
+
+            try
+            {
+                _context.BrojZarazenihOsobas.Update(brZarazenih);
+                _context.SaveChanges();
+                ViewBag.Msg = "Uspesno odradjene izmene";
+                return View();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public IActionResult Details(int id)
+        {
+            BrojZarazenihOsoba brZarazenih = _context.BrojZarazenihOsobas.Find(id);
+            return View(brZarazenih);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            BrojZarazenihOsoba brZarazenih = _context.BrojZarazenihOsobas.Find(id);
+            try
+            {
+                _context.BrojZarazenihOsobas.Remove(brZarazenih);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Covid19Info/ViewModels/BrojZarazenihOsobaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19Info.ViewModels
+{
+    public class BrojZarazenihOsobaViewModel
+    {
+        public int? BrojTestiranihOsobaDnevno { get; set; }
+        public int? BrojPotvrdjenihSlucajeva { get; set; }
+        public int? BrojSmrtnihSlucajeva { get; set; }
+        public int? UkupanBrojTestiranihSlucajeva { get; set; }
+        public DateTime? DatumTestiranja { get; set; }
+        public string DeoNaKojiSePodaciOdnose { get; set; }
+    }
+}
